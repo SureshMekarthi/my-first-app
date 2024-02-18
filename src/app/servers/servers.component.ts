2d041534ad2767066986201b7d55d532141aa978
AnushaPoosala <anusha.poosala@outlook.com>
2024-02-17 22:53:17 -0500

    directives

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,9 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, EnvironmentInjector, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
-  template: `<app-server></app-server><app-server></app-server>`,
+  // template: `<app-server></app-server><app-server></app-server>`,
+  templateUrl:'./servers.component.html'
 })
-export class ServersComponent {
+export class ServersComponent implements OnInit {
+allowNewServer= false;
+serverCreationStatus='No server was created!'
+serverName='';
+serverCreated=false;
 
+constructor(){
+  setTimeout(()=>
+  {
+    this.allowNewServer=true;
+  }, 2000)
+}
+
+ngOnInit(): void {
+  
+}
+onCreateServer(){
+  this.serverCreated= true;
+  this.serverCreationStatus='Server was created'+this.serverName
+}
+onUpdateServerName(event:Event){
+this.serverName=(<HTMLInputElement> event.target).value;
+}
 }
